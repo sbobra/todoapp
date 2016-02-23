369e7117cc67660c758ef8074f2991777e7e5a1f
Sam Bobra <sam@vsco.co>
2016-02-23 00:59:30 -0800

    Add ListView basics.

--- TodoApplication/app/src/main/java/com/example/sam/todoapplication/MainActivity.java
+++ TodoApplication/app/src/main/java/com/example/sam/todoapplication/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.sam.todoapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView todoListView;
+    ArrayList<String> items;
+    ArrayAdapter<String> listViewItemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        todoListView = (ListView) findViewById(R.id.todoListView);
+        items = new ArrayList<>();
+        listViewItemsAdapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, items);
+        todoListView.setAdapter(listViewItemsAdapter);
+        listViewItemsAdapter.add("Start adding items! Here's one.");
+        listViewItemsAdapter.add("Here's another!");
     }
 }
